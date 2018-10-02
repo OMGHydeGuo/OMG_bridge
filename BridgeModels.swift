1ec29fdea169beeb2a3cdb34429805afb5547bc1
Hyde Guo <hydeguo@onwardsmg.com>
2018-10-02 15:40:13 +0800

    update code

---
+++ BridgeModels.swift
@@ -0,0 +1,45 @@
+//
+//  BridgeModels.swift
+//  OMG_Pro
+//
+//  Created by Hydeguo on 2018/9/21.
+//  Copyright © 2018 Hydeguo. All rights reserved.
+//
+
+import Foundation
+
+
+struct BridgeResponse : Codable {
+    
+    var status:Int
+    var body:BridgeBody
+}
+
+
+struct BridgeBody : Codable {
+    
+    var _id:String
+    var updatedAt:String
+    var createdAt:String
+    var originName:String
+    var fileName:String
+    var tenant_id:String
+    var thumbnail:String
+    var logo:String
+    var fingerPrints:[FingerPrintsBody]
+    var type:String
+}
+
+struct FingerPrintsBody : Codable {
+    
+    var _id:String
+    var updatedAt:String
+    var createdAt:String
+    var title:String
+    var description:String
+    var linkURL:String
+    var callbackURL:String
+    var fingerPrintReady:Bool
+}
+
+
---
+++ ListeningController.swift
@@ -0,0 +1,8 @@
+//
+//  ListeningController.swift
+//  OMG_bridge
+//
+//  Created by Hydeguo on 2018/10/2.
+//
+
+import Foundation
